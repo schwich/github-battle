602da94e4f9e4718ef04d669cd9546c26c69799a
Jordan Schwichtenberg <jordan.schwichtenber@navy.mil>
2018-01-19 10:32:52 -0800

    Finish implementing api calls

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,22 @@
 var React = require('react');
 
+var api = require('../utils/api');
+
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    var players = ['mdo', 'fat'];
+    var results = api.battle(players);
+    
+    results.then( (data) => {
+      console.log(data);
+    });
+    
+  }
+
   render() {
+    
     return (
       <div>Results</div>
     )
